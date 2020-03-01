95421bf1b53a1679a0f1aae4e2370f8f8809ace4
新和 张 <463927165@qq.com>
2020-03-01 19:24:39 +0800

    update code

---
+++ SerialPortTest/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace SerialPortTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.textBoxAccept = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BaudRate = new System.Windows.Forms.TextBox();
+            this.TransBits = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(147, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "连接";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "发送";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(406, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "接收";
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Location = new System.Drawing.Point(147, 87);
+            this.textBoxSend.Multiline = true;
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(220, 199);
+            this.textBoxSend.TabIndex = 4;
+            // 
+            // textBoxAccept
+            // 
+            this.textBoxAccept.Location = new System.Drawing.Point(408, 87);
+            this.textBoxAccept.Multiline = true;
+            this.textBoxAccept.Name = "textBoxAccept";
+            this.textBoxAccept.ReadOnly = true;
+            this.textBoxAccept.Size = new System.Drawing.Size(228, 199);
+            this.textBoxAccept.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(147, 304);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.ValueMember = "PortName";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(333, 304);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(507, 304);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 20);
+            this.comboBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 307);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "串口";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(274, 307);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "奇偶校验";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(460, 307);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "停止位";
+            // 
+            // BaudRate
+            // 
+            this.BaudRate.Location = new System.Drawing.Point(147, 330);
+            this.BaudRate.Name = "BaudRate";
+            this.BaudRate.Size = new System.Drawing.Size(100, 21);
+            this.BaudRate.TabIndex = 14;
+            this.BaudRate.Text = "9600";
+            // 
+            // TransBits
+            // 
+            this.TransBits.Location = new System.Drawing.Point(312, 330);
+            this.TransBits.Name = "TransBits";
+            this.TransBits.Size = new System.Drawing.Size(100, 21);
+            this.TransBits.TabIndex = 15;
+            this.TransBits.Text = "8";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(100, 333);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "波特率";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(253, 333);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "传输位数";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(228, 380);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "断开";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TransBits);
+            this.Controls.Add(this.BaudRate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBoxAccept);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "串口测试工具极简版";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.TextBox textBoxAccept;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox BaudRate;
+        private System.Windows.Forms.TextBox TransBits;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SerialPortTest/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialPortTest
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port;
+        Thread thread;
+        bool _continue;
+        public Form1()
+        {
+            _continue = false;
+            InitializeComponent();
+        }
+        public void CreateNewPort()
+        {
+            try
+            {
+                port = new SerialPort(comboBox1.SelectedItem.ToString(), Convert.ToInt32(BaudRate.Text), (Parity)Enum.Parse(typeof(Parity), comboBox2.Text, true),
+                    Convert.ToInt32(TransBits.Text), (StopBits)Enum.Parse(typeof(StopBits), comboBox3.Text, true));
+                port.ReadTimeout = 500;
+                port.WriteTimeout = 500;  //无timeout会一直运行导致连接无法断开
+                _continue = true;
+                //textBoxAccept.AppendText("portLink:"+port.PortName + port.BaudRate.ToString() + port.Parity.ToString() + port.DataBits.ToString() + port.StopBits.ToString() + "\n");
+                port.Open();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            if (!port.IsOpen)
+            {
+                textBoxAccept.AppendText("Cant open the port.\n");
+                return;
+            }
+            thread = new Thread(ReceiveMessage);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+        private void ReceiveMessage()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    String s = port.ReadLine();
+                    if (s != null)
+                    {
+                        string time = DateTime.Now.ToString("[yyyy-MM-dd hh:mm:ss.fff]");
+                        s = time + "[Accept]" + s + "\n";
+                        textBoxAccept.AppendText(s);
+                    }
+                }
+                catch (TimeoutException) { }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (port!=null&&port.IsOpen)
+            {
+                textBoxAccept.AppendText("Port has been opened.\n");
+                return;
+            }
+            CreateNewPort();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String s = textBoxSend.Text;
+            port.WriteLine(s);
+            string time = DateTime.Now.ToString("[yyyy-MM-dd hh:mm:ss.fff]");
+            textBoxAccept.AppendText(time + "[Send]" + s+"\n");
+            textBoxSend.Clear();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (port!=null&&port.IsOpen) port.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+            foreach (string s in Enum.GetNames(typeof(Parity)))
+            {
+                comboBox2.Items.Add(s);
+
+            }
+            foreach (string s in Enum.GetNames(typeof(StopBits)))
+            {
+                comboBox3.Items.Add(s);
+            }
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 1;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (port != null && port.IsOpen)
+            {
+                try
+                {
+                    _continue = false;
+                    thread.Join();
+                    Thread.Sleep(10); 
+                    port.Close();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+    }
+    
+}
